aa6962d49ed1617b424151ffa834ee9752416b88
Igor de Alcantara Barroso <idabmat@gmail.com>
2019-10-19 03:38:31 -0300

    Add navbar to index page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 
-function App() {
+import NavBar from './components/navbar'
+
+const avatarUrl = 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80'
+const userName = 'Jane Doe'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar avatarUrl={avatarUrl} userName={userName} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
