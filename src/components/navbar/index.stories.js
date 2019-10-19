9ee878a6971dd9cbdb45fe958afdd50be5d02549
Igor de Alcantara Barroso <idabmat@gmail.com>
2019-10-19 03:02:37 -0300

    Remove padding in stories

--- src/components/navbar/index.stories.js
+++ src/components/navbar/index.stories.js
@@ -3,10 +3,7 @@ import React from 'react'
 import NavBar from '.'
 
 export default {
-  title: 'NavBar',
-  decorators: [
-    (story) => <div className='-m-6'>{story()}</div>
-  ]
+  title: 'NavBar'
 }
 
 export const defaultNavBar = () => <NavBar />
